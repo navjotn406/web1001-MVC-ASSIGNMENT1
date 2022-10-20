727f264c9fa4989298da4cb461b086d9289727e9
navjotn406 <113278709+navjotn406@users.noreply.github.com>
2022-10-20 17:13:48 -0400

    first commit

---
+++ web1001-MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace web1001_MVC.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
